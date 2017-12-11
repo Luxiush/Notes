3efaa931dceb22ad8129440c572439aac8fe2715
Luxiush <luxiush@qq.com>
2017-12-11 13:44:25 +0800

    exercise2 update

---
+++ algorithms/exercise2/gen_index.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# 提取源文件开头的说明
+
+cd ./
+f_out=./index.md
+if [ -e "$f_out" ]; then rm $f_out; fi # '['和']'前后的空格不能少
+
+fs=`ls *.cpp` # 等号两边不能有空格
+for f in $fs
+do
+    echo " "  >> $f_out
+    echo --- >> $f_out
+    echo $f  >> $f_out
+    sed -n -e '/^\/\*/,/^\*\//p' $f | sed -e '/\/\*/d' -e '/\*\//d' >> $f_out
+done
--- algorithms/exercise2/sum_n.cpp
+++ algorithms/exercise2/sum_n.cpp
@@ -1,17 +1,17 @@
 /*
-## ��1+2+3+...+n
-### ��Ŀ: 
-��1+2+3+...+n��Ҫ����ʹ�ó˳�����for��while��if��else��switch��case�ȹؼ��ּ������ж���䣨A?B:C����
-### [�ⷨ]( http://blog.csdn.net/shoulinjun/article/details/20564095 ): 
-#### �ݹ�: 
-* A||B: �� AΪtrueʱ��B����ִ��.
-* ������һ����ģ��ѡ�����,ʵ�ֵݹ�.
+## 求1+2+3+...+n
+### 题目:
+求1+2+3+...+n，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+### [解法]( http://blog.csdn.net/shoulinjun/article/details/20564095 ):
+#### 递归:
+* A||B: 当 A为true时，B不再执行.
+* 利用这一性质模拟选择语句,实现递归.
 
-#### ģ��
+#### 模板
 ...
 
-#### ��
-* ����C++���е�static����,�Լ����캯���ڶ�����ʱ�ᱻ�Զ����õ����ʡ�
+#### 类
+* 采用C++类中的static变量,以及构造函数在对象构造时会被自动调用的性质。
 */
 #include <iostream>
 
@@ -20,7 +20,7 @@ using namespace std;
 /*******/
 int Sum(int n) {
 	int res = 0;
-	// A || B: �� AΪtrueʱ��B����ִ��Ҳ������ѡ�������
+	// A || B: 当 A为true时，B不再执行也就起到了选择的作用
 	(n==0)||(res=Sum(n-1));
 	return res+n;
 }
@@ -29,7 +29,7 @@ int Sum(int n) {
 class MyClass{
 	static int n;
 	static int sum;
-	
+
 	MyClass(){
 		n += 1;
 		sum += n;
@@ -41,13 +41,13 @@ class MyClass{
 
 int main(){
 	cin>> n;
-	
+
 	cout<< "Solutioon 1: "<< Sum(n)<< endl;
-	
+
 	int MyClass::n = 0;
 	int MyClass::sum = 0;
-	MyClass a[n];  // ����n�ι��캯��
+	MyClass a[n];  // 调用n次构造函数
 	cout<< "Solutioon 2: "<< MyClass::sum<< endl;
-	
+
 	return 0;
 }
